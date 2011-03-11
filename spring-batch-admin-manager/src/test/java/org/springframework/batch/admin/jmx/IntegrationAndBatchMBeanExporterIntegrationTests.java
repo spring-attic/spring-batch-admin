f75e1ed2944128cb28b0c78f3e55dbc1ab51a1fd
Dave Syer <dsyer@vmware.com>
2011-03-11 13:36:07 +0000

    INT-1831: workarounds

---
+++ spring-batch-admin-manager/src/test/java/org/springframework/batch/admin/jmx/IntegrationAndBatchMBeanExporterIntegrationTests.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2006-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.batch.admin.jmx;
+
+import org.junit.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.util.ClassUtils;
+
+/**
+ * @author Dave Syer
+ * 
+ */
+public class IntegrationAndBatchMBeanExporterIntegrationTests {
+
+	@Test
+	public void testMBeanExporters() throws Exception {
+		String base = "/META-INF/spring/batch/";
+		String bootstrap = base + "bootstrap/";
+		// TODO: Fails if integration JMX context is included...
+		new GenericXmlApplicationContext( //
+				ClassUtils.addResourcePathToPackagePath(getClass(), getClass().getSimpleName() + "-context.xml"), //
+				bootstrap + "manager/execution-context.xml", //
+				bootstrap + "manager/jmx-context.xml", //
+				bootstrap + "manager/env-context.xml", //
+				bootstrap + "manager/data-source-context.xml", //
+				bootstrap + "integration/jmx-context.xml" //
+		);
+	}
+
+}
